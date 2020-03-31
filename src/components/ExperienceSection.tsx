cbf0bda13beb3dc24f9c02a0cf0c946040497d92
Li Qian <ericqianli@gmail.com>
2020-03-31 15:55:55 -0700

    save

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -6,7 +6,7 @@ import SectionHeader from "./SectionHeader";
 
 const experiences = [
     {
-        name: "Facebook • 2013 - Present",
+        name: "Facebook • 2013 - 2019",
         title: "Senior Software Engineer",
         description:
             "Building next generation big data analytics platforms and products to empower core business decision making. Built the universal query product supporting 10+ data sources, 8k+ daily active users and 80k+ daily queries. Constantly contributing to various data exploration, visualization and dashboarding products. Led multiple cross team projects, utilizing machine learning to improve query composition, interfacing query platform to anomaly detection and automating interactive dashboard creation. Actively interviewing and training employees on internal data products."
